148779247929a311ba42f17946ace612237f64e1
Nathan Simpson <phinate@protonmail.com>
2022-06-06 22:46:11 +0200

    total change

--- src/neos/__init__.py
+++ src/neos/__init__.py
@@ -1,6 +1,6 @@
 from neos._version import version as __version__
 
-__all__ = ("__version__", "Pipeline", "experiments")
+__all__ = ("__version__", "hists_from_nn", "loss_from_model", "losses",)
 
-from neos import experiments
-from neos.pipeline import Pipeline
+from neos.top_level import hists_from_nn, loss_from_model
+from neos import losses
